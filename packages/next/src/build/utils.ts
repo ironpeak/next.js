6f4f734a28e2026b92f352440b0234c5ab85c89c
Hrafn Orri Hrafnkelsson <hrafn@vidfjord.is>
2024-09-11 16:21:30 +0000

    don't symlink

--- packages/next/src/build/utils.ts
+++ packages/next/src/build/utils.ts
@@ -1969,19 +1969,7 @@ export async function copyTracedFiles(
           copiedFiles.add(fileOutputPath)
 
           await fs.mkdir(path.dirname(fileOutputPath), { recursive: true })
-          const symlink = await fs.readlink(tracedFilePath).catch(() => null)
-
-          if (symlink) {
-            try {
-              await fs.symlink(symlink, fileOutputPath)
-            } catch (e: any) {
-              if (e.code !== 'EEXIST') {
-                throw e
-              }
-            }
-          } else {
-            await fs.copyFile(tracedFilePath, fileOutputPath)
-          }
+          await fs.copyFile(tracedFilePath, fileOutputPath)
         }
 
         await copySema.release()
